6d1b939c0885b71df4dbc4c929feaafa27cd8a4a
Kenneth <sally@sallyde-MacBook-Air.local>
2019-12-14 01:10:49 -0800

    create a appbar for login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navigation from "./Components/Navigation"
 
 function App() {
   return (
-    <div className="App">
-      This is the Home Page
-    </div>
+      <Navigation/>
   );
 }
 
